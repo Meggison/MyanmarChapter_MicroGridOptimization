0d217b55099c584b2b4267c8cc3ef05324d43bab
Ryan Young <rayoung1093@gmail.com>
2024-09-08 19:31:28 -0400

    feat: added diesel capacity optimization

--- src/model_development/optimization/capacity/PSO.py
+++ src/model_development/optimization/capacity/PSO.py
@@ -4,13 +4,60 @@ notes:
 * Given a load and PV generation data per unit capacity, this script optimizes the battery and solar capacity with the objective to minimize installation costs
 * This is loosely based off the equations at the end of "Rural electrification through village grids - Assessing the cost
 competitiveness of isolated renewable energy technologies in Indonesia": https://ethz.ch/content/dam/ethz/special-interest/gess/energy-politics-group-dam/documents/Journal%20Articles/Blum%20et%20al_2013_Renewable%20and%20Sustainable%20Energy%20Reviews.pdf
+
+studies:
+* Minigrids in the Money: https://rmi.org/insight/minigrids-money/
+    - lifetime: 20 years
+    - diesel fuel: $0.8/L
+    - diesel capex: $1050/kW
+    - pv capex: $1020/kW
+    - battery capex: $437/kW
+* Assessing the cost competitiveness of isolated renewable energy technologies in Indonesia: https://ethz.ch/content/dam/ethz/special-interest/gess/energy-politics-group-dam/documents/Journal%20Articles/Blum%20et%20al_2013_Renewable%20and%20Sustainable%20Energy%20Reviews.pdf
+    - lifetime: 20 years (diesel), 25 years (pv), 5 years (battery)
+    - diesel capex: $482/kW
+* Technoeconomic Assessment of Microgrids in Myanmar: https://www.eria.org/uploads/media/ERIA_DP_2018_05.pdf
+    - lifetime: 20 years (diesel), 30 years (pv), 7-11 years (battery)
+    - diesel capex: $490/kW
+    - diesel opex: $0.026/kW
+    - pv capex: $2707/kW - $1600/kW
+    - pv opex: 1% capex - $10/kW
+    - battery capex: $286/kW - $124/kW
+    - battery opex: $0 - $0.04/kW
+* World Energy Outlook: https://iea.blob.core.windows.net/assets/86ede39e-4436-42d7-ba2a-edf61467e070/WorldEnergyOutlook2023.pdf
+    - pv capex: $720/kW (china)
+
+
+1 liter of diesel fuel = 38 MJ OR 10kWh: https://www.bts.gov/content/energy-consumption-mode-transportation-0#:~:text=Diesel%20motor%20fuel%20%3D%2038%2C290%20kJ%2Fliter.
+liters required per kWh = 1 kWh / (EFFICIENCY * 10kwh)
+cost per kWh = liters required per kWh * diesel price per liter
+
+diesel price SE Asia: https://www.rhinocarhire.com/World-Fuel-Prices.aspx ~0.97 euros/L
+diesel price MYANMAR: https://www.globalpetrolprices.com/Burma-Myanmar/diesel_prices/ ~0.83$/L
+
+
 """
+
+from functools import lru_cache
+import time
 import numpy as np
 import pandas as pd
 from scipy.optimize import differential_evolution
 
-# Constants
-eta_batt = 0.8123  # Battery charging efficiency
+SIMULATION_YEARS = 20
+
+# Battery Constants
+RTE_BATT = 0.95  # round trip efficiency of the battery
+CHARGE_EFFICIENCY = RTE_BATT**0.5  # used for both charge and discharge
+MAX_DISCHARGE = 0.9  # Maximum discharge of the battery
+BATTERY_COST = 140
+battery_initial_soc = 1  # Initial state of charge
+
+# PV Constants
+PV_COST = 720
+
+# Diesel Constants
+DIESEL_COST = 261
+DIESEL_FUEL = 0.2
 
 # >>> REPLACE THESE WITH YOUR PATHS !!! <<<
 LOAD_DATA_PATH = "./samples/Tanintharyi_13.1304_98.8394_131.csv"
@@ -25,133 +72,158 @@ load_ts = hourly_load["kW"].values
 pv_ts = pd.read_csv(PV_DATA_PATH)["electricity"].values
 
 # Example load and PV generation data per unit capacity
-# E_PV here represents the energy generated per unit of PV capacity over time [Wh/W]
-hours = 24 * 2
-E_load = load_ts[:hours]  # Energy load over time [Wh]
-E_PV = pv_ts[:hours]  # Energy generated per unit PV capacity [Wh/W]
-# Battery initial state of charge
-battery_initial_soc = 0  # Initial state of charge
+# E_PV here represents the energy generated per unit of PV capacity over time [kWh/kW]
+hours = 24 * 7 * 4 * 3
+E_load = load_ts[:hours]  # Energy load over time [kWh]
+E_PV = pv_ts[: len(E_load)]  # Energy generated per unit PV capacity [kWh/kW]
 
-
-def energy_balance(PV_capacity, battery_capacity):
+@lru_cache
+def energy_balance(pv_capacity, battery_capacity, diesel_capacity):
     """Calculate energy balance over the time period.
 
     Args:
-        PV_capacity (float): PV capacity [W]
-        battery_capacity (float): Battery capacity [W]
+        pv_capacity (float): PV capacity [kW]
+        battery_capacity (float): Battery capacity [kW]
 
     Returns:
         E_batt (np.array): Battery energy balance [Wh]
     """
-    PV_output = PV_capacity * E_PV  # Actual energy produced by the PV system [Wh]
+    PV_output = pv_capacity * E_PV  # Actual energy produced by the PV system [Wh]
     E_batt = np.zeros_like(E_load)
-    soc = battery_initial_soc  # State of charge starts at initial SOC
-
+    C_batt = np.zeros_like(E_load)
+    E_diesel = np.zeros_like(E_load)
+    # State of charge starts at initial SOC
+    soc = battery_initial_soc * battery_capacity
+    max_battery_discharge = MAX_DISCHARGE * battery_capacity
     for t in range(len(E_load)):
         surplus = PV_output[t] - E_load[t]
 
         if surplus > 0:
             # Charge battery with surplus
-            soc += eta_batt * surplus
+            soc += CHARGE_EFFICIENCY * surplus
             soc = min(soc, battery_capacity)  # Cap at battery capacity
         else:
             # Discharge battery to meet deficit
-            soc += surplus
-            soc = max(soc, 0)  # SOC cannot go below 0
-            E_batt[t] = -surplus if soc > 0 else 0
+            available = soc - max_battery_discharge
+            discharged = min(available, -surplus / CHARGE_EFFICIENCY)
+            if discharged > 0:
+                soc -= discharged
+            final_discharge = discharged * CHARGE_EFFICIENCY
+            E_batt[t] = max(final_discharge, 0)
+            surplus += final_discharge
 
-    return E_batt
+        if surplus < 0:
+            E_diesel[t] = min(-surplus, diesel_capacity)
+        C_batt[t] = soc
+
+    return E_batt, E_diesel, C_batt
 
 
 def cost_func(x):
     """Objective function to minimize.
 
     Args:
-        x (np.array): Battery and PV capacity [W]
+        x (np.array): Battery and PV capacity [kW]
 
     Returns:
         float: Total cost
     """
-    PV_capacity = x[0]
+    pv_capacity = x[0]
     battery_capacity = x[1]
+    diesel_capacity = x[2]
 
-    PV_capacity_cost = (
-        PV_capacity * 5
-    )  # Example cost per W for PV (replace with real data)
-    battery_capacity_cost = (
-        battery_capacity * 1
-    )  # Example cost per Wh for battery (replace with real data)
+    pv_capacity_cost = pv_capacity * PV_COST
+    battery_capacity_cost = battery_capacity * BATTERY_COST
+    diesel_capacity_cost = diesel_capacity * DIESEL_COST
 
-    # Total cost
-    total_cost = (PV_capacity_cost + battery_capacity_cost)
-    # LCOE was not giving me good results, so I simplified it to to installation costs only
-    return total_cost
+    # levelized cost of energy (LCOE)
+    _, E_diesel, _ = energy_balance(pv_capacity, battery_capacity, diesel_capacity)
+    # the cost of renewable energy will not be immediately visible with few demand observations
+    # so we need to scale the demand with a load factor to see the long term benefit
+    # otherwise it will pick full conventional generation and not the renewable energy
+    adjusted_demand = SIMULATION_YEARS * 8760
+    load_factor = 1 / (len(E_load) / adjusted_demand)
+    total_cost = (
+        pv_capacity_cost
+        + battery_capacity_cost
+        + diesel_capacity_cost
+        + np.sum(E_diesel * load_factor * DIESEL_FUEL)
+    )
+    return total_cost / np.sum(E_load * load_factor)
 
 
 # Constraints: Ensure demand is met
 def demand_constraint(x):
     """Checks if the demand is met.
     Args:
-        x (np.array): Battery and PV capacity [W]
+        x (np.array): Battery and PV capacity [kW]
 
     Returns:
         float: Total cost
     """
-    PV_capacity = x[0]
+    pv_capacity = x[0]
     battery_capacity = x[1]
-    E_BAT = energy_balance(PV_capacity, battery_capacity)
-    return np.min(E_BAT + PV_capacity * E_PV - E_load)
+    diesel_capacity = x[2]
+    E_BAT, E_diesel, _ = energy_balance(pv_capacity, battery_capacity, diesel_capacity)
+    return np.min(E_BAT + E_diesel + pv_capacity * E_PV - E_load)
 
 
 def constrained_cost(x):
     """Objective function that penalizes if constraints are violated.
     Args:
-        x (np.array): Battery and PV capacity [W]
+        x (np.array): Battery and PV capacity [kW]
 
     Returns:
         float: Total cost
     """
     constraint_violation = demand_constraint(x)
-    if constraint_violation < 0:
+    if constraint_violation < -0.0001:
         # Apply a large penalty if the constraint is violated
         return np.inf
     return cost_func(x)
 
 
-# Initial guess for the optimization
-x0 = [550, 300]  # Initial guesses for [PV capacity, Battery capacity]
+if __name__ == "__main__":
+    # start timer
+    start = time.time()
+    bounds = [(0, 1000), (0, 5000), (0, 1000)]  # Lower and upper bounds
 
-bounds = [(0, 1000), (0, 5000)]  # Lower and upper bounds
-
-# Attempt optimization with different methods or tweaks
-result = differential_evolution(
-    constrained_cost, bounds, strategy="best1bin", maxiter=1000, popsize=15, tol=1e-7
-)
-
-# Set print options for prettier output
-np.set_printoptions(precision=1, suppress=True)
-
-# Check if the optimization was successful
-if result.success:
-    optimal_PV_capacity = result.x[0]
-    optimal_battery_capacity = result.x[1]
-    print(f"Optimal PV capacity: {optimal_PV_capacity} W")
-    print(f"Optimal battery capacity: {optimal_battery_capacity} W")
-    print(f"Minimum Cost: {result.fun}")
-    df = pd.DataFrame(
-        {
-            'Load': E_load,
-            "Energy produced": optimal_PV_capacity * E_PV,
-            "Energy balance": energy_balance(
-                optimal_PV_capacity, optimal_battery_capacity
-            ),
-            "Load met": energy_balance(optimal_PV_capacity, optimal_battery_capacity)
-            + optimal_PV_capacity * E_PV
-            - E_load,
-        }
+    # Attempt optimization with different methods or tweaks
+    result = differential_evolution(
+        constrained_cost, bounds, maxiter=5000, popsize=15, tol=1e-7, workers=-1
     )
-    # print(df.head(50))
 
-else:
-    print(f"Optimization failed: {result.message}")
-    print(f"Last result: {result.x}")
+    # end timer
+    end = time.time()
+    print(f"Time taken: {end - start} seconds")
+
+    # Set print options for prettier output
+    np.set_printoptions(precision=1, suppress=True)
+
+    # Check if the optimization was successful
+    if result.success:
+        optimal_pv_capacity = result.x[0]
+        optimal_battery_capacity = result.x[1]
+        optimal_diesel_capacity = result.x[2]
+        print(f"Optimal pv capacity: {optimal_pv_capacity} kW")
+        print(f"Optimal battery capacity: {optimal_battery_capacity} kW")
+        print(f"Optimal diesel capacity: {optimal_diesel_capacity} kW")
+        print(f"Minimum Cost: {result.fun}")
+        E_Batt, E_diesel, C_batt = energy_balance(
+            optimal_pv_capacity, optimal_battery_capacity, optimal_diesel_capacity
+        )
+        df = pd.DataFrame(
+            {
+                "E_PV": optimal_pv_capacity * E_PV,
+                "E_Batt": E_Batt,
+                "E_diesel": E_diesel,
+                "C_batt": C_batt,
+                "E_load": E_load,
+            }
+        )
+        df['Loss'] = df['E_load'] - df['E_PV'] - df['E_diesel'] - df['E_Batt']
+        df.head()
+
+    else:
+        print(f"Optimization failed: {result.message}")
+        print(f"Last result: {result.x}")
